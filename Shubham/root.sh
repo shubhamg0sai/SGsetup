36c473c62ca266d0a570c7e6f5c3355e1ec6e7b6
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-02-14 18:51:01 +0530

    Update root.sh

--- Shubham/root.sh
+++ Shubham/root.sh
@@ -3,16 +3,16 @@ p='\033[1;35m'
 clear
 echo -e "\033[1;32m"
 echo "    {Ctrl C} exit "
-echo " Facebook tools "
+echo " termux root tools tools "
 echo "N[1] imstall root termux arch"                                                                 
 echo "N[2] imstall root termux kalilinux". 
 echo "N[3] install root termux ubantu v1"
 echo "N[4] install root termux ubantu v2"  
-echo "N[5]   about me"
+echo "N[5] install fake root termux "  
+echo "N[6]   about me"
 echo "N[0]   back to home"     
 echo "N[00]  exit tool"                                   
 echo ""
-echo -e "$p"
 read -p  "number-------> " N
 
 
@@ -153,6 +153,39 @@ elif [ "$N" -eq "4"  ]; then
 
 
 elif [ "$N" -eq "5"  ]; then
+ cd
+ clear
+ termux-setup-storage
+ clear
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ clear 
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+ sleep 0.4
+ cd
+ git clone https://github.com/MaulanaRyM/FakeRoot
+ cd FakeRoot
+ chmod +x *
+ clear
+ echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+ sleep 2 
+ python2 root.py
+ echo "    {Ctrl C} exit "
+ sleep 2
+ cd
+ git clone https://github.com/ShuBhamg0sain/SGsetup
+ cd SGsetup
+ cd Shubham
+ bash root.sh
+
+
+elif [ "$N" -eq "6"  ]; then
  cd
  clear
  clear
