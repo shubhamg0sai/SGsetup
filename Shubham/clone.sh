fbae246e300c79e0681375d3a8c237ddacf62436
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-02-14 16:25:29 +0530

    Update clone.sh

--- Shubham/clone.sh
+++ Shubham/clone.sh
@@ -3,18 +3,19 @@ p='\033[1;35m'
 clear
 echo -e "\033[1;32m"
 echo "    {Ctrl C} exit "
-echo "this is the page 3 of tool-s-installer"
-echo "N[1] imstall fb-cloning-id"                                                                 
-echo "N[2] imstall FB-check-point".  
-echo "N[3] install phonehack"                                                                       
-echo "N[4] install phone-number-tracker"        
-echo "N[5] install hack-wifi"                                                                       
-echo "N[6] install Facebook_hack"
+echo "welcome to cloning tool for facebook"
+echo "N[1] imstall fb-cloning login"                                                                   
+echo "N[2] install Inallsim v1"                                                                       
+echo "N[3] install Inallsim v2"        
+echo "N[4] install Worldsallsim v1"                                                                       
+echo "N[5] install Worldsallsim v2"
 echo "[0]   back page "                                            
 
 echo ""
 echo -e "$p"
 read -p  "number-------> " N
+
+
 
 if [ "$N" -eq "1"  ]; then
  cd
@@ -35,11 +36,12 @@ if [ "$N" -eq "1"  ]; then
  cd
  git clone https://github.com/ShuBhamg0sain/fb-cloning-id
  cd fb-cloning-id
- chmod +x * Shubham.py
+ pip2 install requests
+ pip2 install mechanize
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- python2 Shubham.py
+ python2 ShuBham.py
 echo "    {Ctrl C} exit "
  sleep 2
  cd
@@ -67,20 +69,22 @@ elif [ "$N" -eq "2"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/fb-cloning-id
- cd fb-cloning-id
- chmod +x * Shubham.py
+ pip2 install requests
+ pip2 install mechanize
+ git clone https://github.com/ShuBhamg0sain/Inallsim-v1
+ cd Inallsim-v1
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- python2 Shubham.py
-echo "    {Ctrl C} exit "
+ python2 S.py
+ echo "    {Ctrl C} exit "
  sleep 2
  cd
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  cd Shubham
  bash clone.sh
+
 
 
 elif [ "$N" -eq "3"  ]; then
@@ -100,13 +104,14 @@ elif [ "$N" -eq "3"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/fb-cloning-id
- cd fb-cloning-id
- chmod +x * Shubham.py
+ pip2 install requests
+ pip2 install mechanize
+ git clone https://github.com/ShuBhamg0sain/Inallsim-v2
+ cd Inallsim-v2
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- python2 Shubham.py
+ python2 S.py
 echo "    {Ctrl C} exit "
  sleep 2
  cd
@@ -134,20 +139,22 @@ elif [ "$N" -eq "4"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/fb-cloning-id
- cd fb-cloning-id
- chmod +x * Shubham.py
+ pip2 install requests
+ pip2 install mechanize
+ git clone https://github.com/ShuBhamg0sain/worldsallsim-v1
+ cd worldsallsim-v1
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- python2 Shubham.py
-echo "    {Ctrl C} exit "
+ python2 S.py
+ echo "    {Ctrl C} exit "
  sleep 2
  cd
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  cd Shubham
  bash clone.sh
+
 
 
 elif [ "$N" -eq "5"  ]; then
@@ -167,54 +174,21 @@ elif [ "$N" -eq "5"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/fb-cloning-id
- cd fb-cloning-id
- chmod +x * Shubham.py
+ pip2 install requests
+ pip2 install mechanize
+ git clone https://github.com/ShuBhamg0sain/worldsallsim-v2
+ cd worldsallsim-v2
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- python2 Shubham.py
-echo "    {Ctrl C} exit "
+ python2 S.py
+ echo "    {Ctrl C} exit "
  sleep 2
  cd
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  cd Shubham
  bash clone.sh
-
-
-elif [ "$N" -eq "6"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/fb-cloning-id
- cd fb-cloning-id
- chmod +x * Shubham.py
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- python2 Shubham.py
-echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash clone.sh
-
 
 
 
@@ -236,6 +210,8 @@ elif [ "$N" -eq "0"  ]; then
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  bash Sg.sh
+
+
 
 else :
  bash clone.sh
