db09132c94e0ffdc134d1fd404e42365bfc0e17f
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-02-14 12:15:30 +0530

    Update Sg.sh

--- Sg.sh
+++ Sg.sh
@@ -4,15 +4,15 @@ clear
 echo -e "\033[1;32m"
 echo "    {Ctrl C} exit "
 echo "this is the page 1 of tool-s-installer"
-echo "N[1] imstall fb-cloning-id"                                                                 
-echo "N[2] imstall FB-check-point".  
-echo "N[3] install phonehack"                                                                       
-echo "N[4] install phone-number-tracker"        
-echo "N[5] install hack-wifi"                                                                       
-echo "N[6] install Facebook_hack"
-echo "N[7] install basic-installtion-comands"                                                       
-echo "N[8] install ShuBham"          
-echo "N[9] install "                                                                                
+echo "N[1]  all types of bruteForces"                                                                 
+echo "N[2]  FB-check-point".  
+echo "N[3]  phonehack"                                                                       
+echo "N[4] phone-number-tracker"        
+echo "N[5]  hack-wifi"                                                                       
+echo "N[6] Facebook_hack"
+echo "N[7]  basic-installtion-comands"                                                       
+echo "N[8]  ShuBham"          
+echo "N[9] inst "                                                                                
 echo "N[10] install "                          
 echo "N[11] install "               
 echo "N[12] install "
@@ -47,13 +47,14 @@ if [ "$N" -eq "1"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/fb-cloning-id
- cd fb-cloning-id
- chmod +x * Shubham.py
+ git clone https://github.com/ShuBhamg0sain/SGsetup
+ cd SGsetup
+ cd Shubham
+ chmod +x * Brute.sh
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- python2 Shubham.py
+ bash Brute.sh
 echo "    {Ctrl C} exit "
  sleep 2
  cd
