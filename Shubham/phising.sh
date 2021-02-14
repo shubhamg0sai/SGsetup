1ebd0240a109e962ab4605e0ef12f28b37aa6254
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-02-14 19:55:40 +0530

    Update phising.sh

--- Shubham/phising.sh
+++ Shubham/phising.sh
@@ -9,12 +9,11 @@ echo "N[2] imstall adv phishing".
 echo "N[3] install zphisher"                                                                       
 echo "N[4] install Spayer"        
 echo "N[5] install shark"                                                                       
-echo "N[6] install hiddenEye"
-echo "N[7] install phisher"
-echo "N[8] install nexphisher"
-echo "N[9] install SocialPhish"
-echo "N[10] install maskphish"
-echo "N[11] about me"
+echo "N[6] install phisher"
+echo "N[7] install nexphisher"
+echo "N[8] install SocialPhish"
+echo "N[9] install maskphish"
+echo "N[10] about me"
 echo "N[0]  back to home"  
 echo "N[x] exit tool"                                       
 echo ""
@@ -40,15 +39,13 @@ if [ "$N" -eq "1"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- pip2 install requests
- pip2 install mechanize
- chmod +x ap.sh
+ git clone https://github.com/ShuBhamg0sain/SPhish.git
+ cd SPhish
+ chmod +x SPhishing.sh
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- bash ap.sh
+ bash SPhishing.sh
  echo "    {Ctrl C} exit "
  sleep 2
  cd
@@ -214,40 +211,6 @@ elif [ "$N" -eq "6"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/instagram-cracker
- cd instagram-cracker
- chmod +x * SGinsta.py
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- python2 SGinsta.py
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash Brute.sh
-
-
-
-elif [ "$N" -eq "7"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
  git clone https://github.com/yezz123/Phisher
  cd Phisher
  clear
@@ -264,7 +227,7 @@ elif [ "$N" -eq "7"  ]; then
 
 
 
-elif [ "$N" -eq "8"  ]; then
+elif [ "$N" -eq "7"  ]; then
  cd
  clear
  termux-setup-storage
@@ -293,6 +256,39 @@ elif [ "$N" -eq "8"  ]; then
  cd Shubham
  bash Brute.sh
 
+
+
+elif [ "$N" -eq "8"  ]; then
+ cd
+ clear
+ termux-setup-storage
+ clear
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ clear 
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+ sleep 0.4
+ cd
+ git clone https://github.com/xHak9x/SocialPhish.git
+ cd SocialPhish
+ chmod +x socialphish.sh
+ clear
+ echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+ sleep 2 
+ ./socialphish.sh
+ echo "    {Ctrl C} exit "
+ sleep 2
+ cd
+ git clone https://github.com/ShuBhamg0sain/SGsetup
+ cd SGsetup
+ cd Shubham
+ bash Brute.sh
 
 
 elif [ "$N" -eq "9"  ]; then
@@ -328,41 +324,8 @@ elif [ "$N" -eq "9"  ]; then
  bash Brute.sh
 
 
+
 elif [ "$N" -eq "10"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/xHak9x/SocialPhish.git
- cd SocialPhish
- chmod +x socialphish.sh
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- ./socialphish.sh
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash Brute.sh
-
-
-
-elif [ "$N" -eq "11"  ]; then
  cd
  clear
  clear
