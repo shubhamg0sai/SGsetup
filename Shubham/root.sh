be75a37387c55b450d7e2528f98534337b152851
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-02-14 18:43:40 +0530

    Update root.sh

--- Shubham/root.sh
+++ Shubham/root.sh
@@ -7,9 +7,8 @@ echo " Facebook tools "
 echo "N[1] imstall root termux arch"                                                                 
 echo "N[2] imstall root termux kalilinux". 
 echo "N[3] install root termux ubantu v1"
-echo "N[3] install root termux ubantu v2"  
-echo "N[3] install root termux "  
-echo "N[4]   about me"
+echo "N[4] install root termux ubantu v2"  
+echo "N[5]   about me"
 echo "N[0]   back to home"     
 echo "N[00]  exit tool"                                   
 echo ""
@@ -47,7 +46,7 @@ if [ "$N" -eq "1"  ]; then
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  cd Shubham
- bash facebook.sh
+ bash root.sh
 
 
 
@@ -81,7 +80,7 @@ elif [ "$N" -eq "2"  ]; then
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  cd Shubham
- bash facebook.sh
+ bash root.sh
 
 
 
@@ -115,11 +114,45 @@ elif [ "$N" -eq "3"  ]; then
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  cd Shubham
- bash facebook.sh
+ bash root.sh
 
 
 
-elif [ "$N" -eq "10"  ]; then
+elif [ "$N" -eq "4"  ]; then
+ cd
+ clear
+ termux-setup-storage
+ clear
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ clear 
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+ sleep 0.4
+ cd
+ git clone https://GitHub.com/ShuBhamg0sain/rootubantu-termux.git
+ cd rootubantu-termux
+ chmod +x *
+ clear
+ echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+ sleep 2 
+ bash ubantu2.0.sh
+ echo "    {Ctrl C} exit "
+ sleep 2
+ cd
+ git clone https://github.com/ShuBhamg0sain/SGsetup
+ cd SGsetup
+ cd Shubham
+ bash root.sh
+
+
+
+elif [ "$N" -eq "5"  ]; then
  cd
  clear
  clear
@@ -147,7 +180,7 @@ elif [ "$N" -eq "10"  ]; then
  git clone https://github.com/ShuBhamg0sain/SGsetup
  cd SGsetup
  cd Shubham
- bash facebook.sh
+ bash root.sh
 
 
 
@@ -185,5 +218,5 @@ elif [ "$N" -eq "00"  ]; then
 
 
 else :
- bash facebook.sh
+ bash root.sh
 fi
