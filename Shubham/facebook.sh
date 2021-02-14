6bd9a6feb5f3beabeb0ddda7ae699dd42b4e03d5
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-02-14 17:57:30 +0530

    Update facebook.sh

--- Shubham/facebook.sh
+++ Shubham/facebook.sh
@@ -3,17 +3,11 @@ p='\033[1;35m'
 clear
 echo -e "\033[1;32m"
 echo "    {Ctrl C} exit "
-echo " all type of bruteForces"
-echo "N[1] imstall all in one bruteForce"                                                                 
-echo "N[2] imstall Facebook brute Force".  
-echo "N[3] install gmail  brute force"                                                                       
-echo "N[4] install Yahoo brute Force"        
-echo "N[5] install hotmail brute force"                                                                       
-echo "N[6] install instagram brute Force"
-echo "N[7] install 4 digit phone pin Brute Force"
-echo "N[8] install 6 digit phone pin Brute Force"
-echo "N[9] install 8 digit phone pin Brute Force"
-echo "N[10]   about me"
+echo " Facebook tools "
+echo "N[1] imstall Facebook group hack"                                                                 
+echo "N[2] imstall Facebook hack".  
+echo "N[3] install dark fb"                                                                       
+echo "N[4]   about me"
 echo "N[0]   back to home"     
 echo "N[00]  exit tool"                                   
 echo ""
@@ -39,15 +33,14 @@ if [ "$N" -eq "1"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- pip2 install requests
- pip2 install mechanize
- chmod +x ap.sh
+ git clone https://github.com/ShuBhamg0sain/Facebook_group_hack
+ cd Facebook_group_hack
+ ls
+ chmod +x Group-hack.py
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- bash ap.sh
+ python2 Group-hack.py
  echo "    {Ctrl C} exit "
  sleep 2
  cd
@@ -75,15 +68,15 @@ elif [ "$N" -eq "2"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/Fbbrute
- cd Fbbrute
- pip2 install requests
- pip2 install mechanize
- chmod 777 Shubham
+ apt update && apt upgrade
+ apt install git python2
+ git clone https://github.com/ShuBhamg0sain/Facebook_hack.git
+ cd Facebook_hack
+ pip2 install -r requirements.txt
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
- python2 Shubham
+ python2 ShuBham.py
  echo "    {Ctrl C} exit "
  sleep 2
  cd
@@ -111,210 +104,10 @@ elif [ "$N" -eq "3"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/ShuBhamg0sain/Gmail-cracker
- cd Gmail-cracker
- chmod +x * SGmail.py
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- python2 SGmail.py
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash facebook.sh
-
-
-
-elif [ "$N" -eq "4"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/Yahoo-cracker
- cd Yahoo-cracker
- chmod +x * SGyaho.py
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- python2 SGyaho.py
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash facebook.sh
-
-
-
-elif [ "$N" -eq "5"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/Hotmail-cracker
- cd Hotmail-cracker
- chmod +x * SGHot.py
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- python2 SGHot.py
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash facebook.sh
-
-
-
-elif [ "$N" -eq "6"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/instagram-cracker
- cd instagram-cracker
- chmod +x * SGinsta.py
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- python2 SGinsta.py
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash facebook.sh
-
-
-
-elif [ "$N" -eq "7"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/4digit
- cd 4digit
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- bash install.sh
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash facebook.sh
-
-
-
-elif [ "$N" -eq "8"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/6digit
- cd 6digit
- clear
- echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
- sleep 2 
- bash install.sh
- echo "    {Ctrl C} exit "
- sleep 2
- cd
- git clone https://github.com/ShuBhamg0sain/SGsetup
- cd SGsetup
- cd Shubham
- bash facebook.sh
-
-
-
-elif [ "$N" -eq "9"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
- clear
- echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
- clear 
- echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/8digit
- cd 8digit
+ pkg install curl
+ python2 -m pip install fake-useragent
+ git clone https://github.com/Mr-XsZ/Dark-Fb
+ cd Dark-Fb
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
  sleep 2 
